e7c0a33d9548e0447ffb29e6b865504a0ba5cbfa
hnrqss <dev@hnrqss.com>
2023-09-06 20:00:58 -0300

    iniciando com listas

---
+++ introducao_python/listas.py
@@ -0,0 +1,15 @@
+minha_lista = ['maça', 'pera', 'uva']
+
+minha_lista.append('valor')
+
+minha_lista.insert(10, 'valor')
+
+minha_lista.remove('valor')
+
+minha_lista.pop(1)
+
+minha_lista[0] = 'valor'
+
+print(minha_lista)
+print(type(minha_lista))
+
