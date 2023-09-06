d8223fd75e6120fc36a21942b602dcb2da7c65d1
hnrqss <dev@hnrqss.com>
2023-09-06 20:49:59 -0300

    organizando codigo

--- introducao_python/Aula-2/exercicios/exercicio_1.py
+++ introducao_python/Aula-2/exercicios/exercicio_1.py
@@ -1,3 +1,18 @@
+
+
+
+# valores das compras
+compras = [10.20, 20, 40.70, 50]
+
+# Dicionario pessoa
+pessoa = {
+    "Nome": "Henrique",
+    "Ano_nascimento": "1990",
+    "Compras": compras,
+}
+
+# Desafio imprimir idade e média das compras
+
 def soma_compras(compras):
     soma = 0
 
@@ -14,22 +29,10 @@ def calcular_idade(ano_nascimento):
     ano_atual = 2023
     return ano_atual - ano_nascimento
 
-
-# valores das compras
-compras = [10.20, 20, 40.70, 50]
-
-# Dicionario pessoa
-pessoa = {
-    "Nome": "Henrique",
-    "Ano_nascimento": "1990",
-    "Compras": compras,
-}
-
 soma = soma_compras(pessoa['Compras'])
 media = calcular_media(soma, pessoa['Compras'])
 idade = calcular_idade(int(pessoa["Ano_nascimento"]))
 
-# Desafio imprimir idade e média das compras
 
 resultado = "Idade: {}\nMédia das compras: {}".format(idade, media)
 
