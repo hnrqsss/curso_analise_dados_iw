1513e72ff80c50fcc67c72fd1db5283da7b681d2
hnrqss <dev@hnrqss.com>
2023-09-13 16:04:22 -0300

    calcular imc

--- introducao_python/Aula-2/exercicios/calculo_imc.py
+++ introducao_python/Aula-2/exercicios/calculo_imc.py
@@ -1,1 +1,18 @@
 # Criar algoritimo para calcular IMC de uma pessoa recebendo os dados necessários através do input
+
+pessoa = {
+    'Nome': '',
+    'Peso': '',
+    'Altura': '',
+}
+
+# peso kg/altura cm ²
+pessoa['Nome'] = input('Digite seu nome: ')
+pessoa['Peso'] = float(input('Digite seu peso em kg: '))
+pessoa['Altura'] = float(input('Digite sua altura em metros: '))
+
+imc = pessoa['Peso']/pessoa['Altura']**2
+
+response = ('Nome {} \nIMC: {}').format(pessoa['Peso'], imc)
+
+print(response)
